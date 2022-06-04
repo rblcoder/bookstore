7be44c067120cdd22cbe34310fb41e8f448e46dc
rblcoder <35212670+rblcoder@users.noreply.github.com>
2022-06-04 17:44:24 +0530

    change name of Configuration class

--- src/main/java/com/company/bookStore/config/Configure.java
+++ src/main/java/com/company/bookStore/config/Configure.java
@@ -2,9 +2,10 @@ package com.company.bookStore.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@org.springframework.context.annotation.Configuration
-public class Configuration {
+@Configuration
+public class Configure {
 
     @Bean
     public ModelMapper getModelMapper() {
