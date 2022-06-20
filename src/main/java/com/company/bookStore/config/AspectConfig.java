8d6a4b69ba5ec7ffd854019ef1fb4884d84cc3f7
rblcoder <35212670+rblcoder@users.noreply.github.com>
2022-06-20 18:34:09 +0530

    aop profiling

--- src/main/java/com/company/bookStore/config/AspectConfig.java
+++ src/main/java/com/company/bookStore/config/AspectConfig.java
@@ -1,12 +1,16 @@
 package com.company.bookStore.config;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StopWatch;
 
 @Aspect
 @Configuration
@@ -23,4 +27,25 @@ public class AspectConfig {
         logger.info("Finished execution of {}", joinPoint);
     }
 
+    @Around("execution(* com.company.bookStore.controller.*.*(..)))")
+    public Object profileAllMethods(ProceedingJoinPoint proceedingJoinPoint) throws Throwable
+    {
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+
+        //Get intercepted method details
+        String className = methodSignature.getDeclaringType().getSimpleName();
+        String methodName = methodSignature.getName();
+
+        final StopWatch stopWatch = new StopWatch();
+
+        //Measure method execution time
+        stopWatch.start();
+        Object result = proceedingJoinPoint.proceed();
+        stopWatch.stop();
+
+        //Log method execution time
+        logger.info("Execution time of " + className + "." + methodName + " :: " + stopWatch.getTotalTimeMillis() + " ms");
+
+        return result;
+    }
 }
